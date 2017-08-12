e1e6d16464d759398ea259316f7a44d833248bc0
antonkov1996 <antonkov1996@gmail.com>
2017-08-12 17:21:01 +0300

    Add Javadoc

--- src/main/java/ru/setup/SetupCopmlete.java
+++ src/main/java/ru/setup/SetupCopmlete.java
@@ -1,6 +1,13 @@
 package ru.setup;
 
+/**
+ * This program created only for setup my plugins
+ */
 public class SetupCopmlete {
+    /**
+     * Method printing 2 lines in console
+     * @param args default params
+     */
     public static void main(String[] args) {
         System.out.println("setup");
         System.out.println("complete");
